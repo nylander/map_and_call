0820324d5c0d2c8c33ad07cdd87b986267034c61
axeljen <axejen@gmail.com>
2026-05-20 10:19:44 +0200

    move alternative entry points to separate directory

--- run_locally.sh
+++ run_locally.sh
@@ -12,34 +12,11 @@
 ## load nextflow module
 #ml nextflow
 
+# only used for testing!
+
 nextflow run main.nf --input ../testfiles/input.csv -profile standard -resume \
     --reference ../testfiles/reference/GCF_003339765.1_Mmul_10_4chroms.fa \
     --reads_dir ../testfiles/reads/supersmall \
     --scaffold_list ../testfiles/test_4chroms.txt \
     --variant_caller freebayes \
     --outdir test/freebayes_new
-
-# nextflow run preprocess_reads.nf --input ../testfiles/input.csv -profile standard -resume \
-#     --reference ../testfiles/reference/GCF_003339765.1_Mmul_10_4chroms.fa \
-#     --reads_dir ../testfiles/reads/supersmall \
-#     --scaffold_list ../testfiles/scaffolds.txt \
-#     --variant_caller bcftools \
-#     --skip_premapping_dedup \
-#     --outdir test/preprocess_test
-
-# nextflow run read_qc.nf -profile standard -resume \
-#     --reference ../testfiles/reference/GCF_003339765.1_Mmul_10_4chroms.fa \
-#     --reads_dir ../testfiles/reads/supersmall \
-#     --scaffold_list ../testfiles/scaffolds.txt \
-#     --variant_caller bcftools \
-#     --outdir test/readqc_test
-
-# nextflow run preprocess_and_map.nf --input ../testfiles/input.csv -profile standard -resume \
-#     --reference test/testout_bcftools/00_input_data/00_reference_genome/GCF_003339765.1_Mmul_10_4chroms.fa \
-#     --reads_dir ../testfiles/reads/supersmall \
-#     --scaffold_list ../testfiles/scaffolds.txt \
-#     --variant_caller bcftools \
-#     --skip_premapping_dedup \
-#     --outdir test/preprocess_and_map_test
-
-# test/testout_bcftools/00_input_data/00_reference_genome/GCF_003339765.1_Mmul_10_4chroms.fa 
